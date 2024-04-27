102566844152103bc05c45df71dbc59b60eaa2b0
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2024-04-27 12:10:15 +0600

    content changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
   ],
+  presets: [keepPreset],
   theme: {
     extend: {
       height: {
