90a652b7f455c315c347d4061e7865aaeeef0260
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2024-04-27 12:04:29 +0600

    something changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
